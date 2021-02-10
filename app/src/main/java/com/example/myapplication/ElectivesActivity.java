5d97b70d83449fb56f37b9c35860ddfd1dfdf684
Egor <giksengik@gmail.com>
2021-02-10 10:47:00 +0300

    work with elective dialogs

--- app/src/main/java/com/example/myapplication/ElectivesActivity.java
+++ app/src/main/java/com/example/myapplication/ElectivesActivity.java
@@ -2,10 +2,15 @@ package com.example.myapplication;
 
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ElectivesActivity extends AppCompatActivity {
+        private Button addNewElectiveButton;
+        private Button searchButton;
+        private EditText searchBoard;
+
         @Override
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
